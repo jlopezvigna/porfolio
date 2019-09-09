313f84d0ff71a83d0918adef6de98ceb23bd8577
Julian <lopezvignajulian@gmail.com>
2019-09-08 23:57:05 -0300

    cambia palabra y fix de identacion

--- js/type.js
+++ js/type.js
@@ -1,43 +1,43 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
 	const mHTML = document.getElementById('message'),
-	messages = [
-	  'Front-end Developer',
-	  'Back-end Developer',
-	  'Passionate about Technology'
-	];
+		messages = [
+			'Front-end Developer',
+			'Back-end Developer',
+			'Freelancer'
+		];
 	let currentMessage = 0;
 	function typeMessage() {
-	  if (!messages[currentMessage]) {
-		currentMessage = 0;
-	  }
-	  const currentStr = messages[currentMessage];
-	  currentStr.split();
-	  let part = '';
-	  let currentLetter = 0;
-	  let int1 = setInterval(()=>{
-		if (!currentStr[currentLetter]) {
-		  currentMessage++;
-		  setTimeout(()=>{ deleteMessage(part); }, 500);
-		  clearInterval(int1);
-		} else {
-		  part += currentStr[currentLetter++];
-		  mHTML.innerHTML = part;
+		if (!messages[currentMessage]) {
+			currentMessage = 0;
 		}
-	  }, 100);
+		const currentStr = messages[currentMessage];
+		currentStr.split();
+		let part = '';
+		let currentLetter = 0;
+		let int1 = setInterval(() => {
+			if (!currentStr[currentLetter]) {
+				currentMessage++;
+				setTimeout(() => { deleteMessage(part); }, 500);
+				clearInterval(int1);
+			} else {
+				part += currentStr[currentLetter++];
+				mHTML.innerHTML = part;
+			}
+		}, 100);
 	}
 
 	function deleteMessage(str) {
-	  let int = setInterval(()=>{
-		if (str.length === 0) {
-		  setTimeout(()=>{ typeMessage(); }, 500);
-		  clearInterval(int);
-		} else {
-		  str = str.split('');
-		  str.pop();
-		  str = str.join('');
-		  mHTML.innerHTML = str;
-		}
-	  },50);
+		let int = setInterval(() => {
+			if (str.length === 0) {
+				setTimeout(() => { typeMessage(); }, 500);
+				clearInterval(int);
+			} else {
+				str = str.split('');
+				str.pop();
+				str = str.join('');
+				mHTML.innerHTML = str;
+			}
+		}, 50);
 	}
 
 	typeMessage();
