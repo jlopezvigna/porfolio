caff8f45e996d273a2d0b8b59872aaeb8b35ce1f
Julian <lopezvignajulian@gmail.com>
2019-09-08 23:54:48 -0300

    elimina console

--- js/app.js
+++ js/app.js
@@ -1,16 +1,12 @@
-document.querySelector('.link-menu1').addEventListener('click', function() {
-    console.log('www11');
+document.querySelector('.link-menu1').addEventListener('click', function () {
     document.getElementById("checkbox").checked = false;
 });
-document.querySelector('.link-menu2').addEventListener('click', function() {
-    console.log('www22');
+document.querySelector('.link-menu2').addEventListener('click', function () {
     document.getElementById("checkbox").checked = false;
 });
-document.querySelector('.link-menu3').addEventListener('click', function() {
-    console.log('www33');
+document.querySelector('.link-menu3').addEventListener('click', function () {
     document.getElementById("checkbox").checked = false;
 });
-document.querySelector('.link-menu4').addEventListener('click', function() {
-    console.log('www44');
+document.querySelector('.link-menu4').addEventListener('click', function () {
     document.getElementById("checkbox").checked = false;
 });
